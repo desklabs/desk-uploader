754c13e8b8a40ec58560adafa965c597c4b9485e
Eric Dahl <eric@skeyelab.com>
2016-09-27 16:22:56 -0500

    update

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem "gibberish"
 gem 'desk_api'
 gem 'sidekiq-superworker'
 
-
+gem 'rails_12factor'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,18 +8,18 @@ class Customer
     password = cipher.encrypt(password)
     x = 0
     #CSV.foreach(file.path, headers: true) do |row|
-    SmarterCSV.process(file.path) do |row|
-      print "."
-      job = {
-        :data => row.first,
-        :domain => domain,
-        :username => username,
-        :password => password
-      }
-      if ProcessInboundCustomerRow.perform_in(10.seconds, job)
-        x += 1
-      end
-    end
+    # SmarterCSV.process(file.path) do |row|
+    #   print "."
+    #   job = {
+    #     :data => row.first,
+    #     :domain => domain,
+    #     :username => username,
+    #     :password => password
+    #   }
+    #   if ProcessInboundCustomerRow.perform_in(10.seconds, job)
+    #     x += 1
+    #   end
+    # end
     return x
   end
 end
