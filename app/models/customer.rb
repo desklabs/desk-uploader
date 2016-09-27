bf4c534d302754c48af1923e3b28eb632537d946
Eric Dahl <eric@skeyelab.com>
2016-09-27 16:33:49 -0500

    speed?

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,18 +7,16 @@ class Customer
     cipher = Gibberish::AES::CBC.new(ENV['SIDEKIQ_ENCRYPTION_KEY'])
     password = cipher.encrypt(password)
     x = 0
-    #CSV.foreach(file.path, headers: true) do |row|
      SmarterCSV.process(file.path) do |row|
-    #   print "."
-    #   job = {
-    #     :data => row.first,
-    #     :domain => domain,
-    #     :username => username,
-    #     :password => password
-    #   }
-      #if ProcessInboundCustomerRow.perform_in(10.seconds, job)
+      job = {
+        :data => row.first,
+        :domain => domain,
+        :username => username,
+        :password => password
+      }
+      if ProcessInboundCustomerRow.perform_in(10.seconds, job)
         x += 1
-      #end
+      end
     end
     return x
   end
