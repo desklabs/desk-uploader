dfb2a42732067347d2cc9ea63c221dcd9ac7d01b
Eric Dahl <eric@skeyelab.com>
2016-09-27 14:03:16 -0500

    remove desk model

--- app/models/desk.rb
+++
@@ -1,8 +0,0 @@
-class Desk
-    include Mongoid::Document
-
-    field :domain, type: String
-    field :username, type: String
-    field :password, type: Mongoid::EncryptedString
-    has_many :customers
-end
