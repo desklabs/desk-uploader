97b2d7076ffedff9a1f03df9e6b40fce659f0b7a
Eric Dahl <eric@skeyelab.com>
2016-10-03 11:42:28 -0500

    streamlining

--- app/workers/process_inbound_csv_file.rb
+++ app/workers/process_inbound_csv_file.rb
@@ -5,7 +5,7 @@ class ProcessInboundCSVFile
   def perform(domain, username, password, uid)
     #   binding.pry
     uploaded_file = UploadedCsvFile.where(id: uid).first
-    jobs = []
+   # jobs = []
     CSV.parse(uploaded_file.file.read, headers: true, :encoding => 'ISO-8859-1') do |row|
       job = {
         :data => row.to_hash,
@@ -13,10 +13,10 @@ class ProcessInboundCSVFile
         :username => username,
         :password => password
       }
-      jobs << job
-  #    ProcessInboundCustomerRow.perform_async(job)
+  #    jobs << job
+      ProcessInboundCustomerRow.perform_async(job)
     end
-    CreateJobsForCSVFile.perform_async(jobs)
+   # CreateJobsForCSVFile.perform_async(jobs)
     uploaded_file.destroy
   end
 
