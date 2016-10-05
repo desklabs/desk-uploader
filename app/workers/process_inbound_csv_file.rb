44f886ccec8610839388acbf5405416edf43b61d
Eric Dahl <eric@skeyelab.com>
2016-10-05 15:59:50 -0500

    less database calls

--- app/workers/process_inbound_csv_file.rb
+++ app/workers/process_inbound_csv_file.rb
@@ -10,7 +10,7 @@ class ProcessInboundCSVFile
     CSV.parse(uploaded_file.file.read, headers: true, :encoding => 'ISO-8859-1') do |row|
       row["external_id"] = row["id"]
       row.delete "id"
-      r = Row.create(:data => row.to_json)
+      r = Row.new(:data => row.to_json)
       uploaded_file.rows.push(r)
       jobs << r.id.to_s
     end
