c50493eb7cc83922a43ae6d48b7c007a4c88d3b4
Eric Dahl <eric@skeyelab.com>
2016-10-05 14:30:38 -0500

    emails!

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifier < ApplicationMailer
-  default :from => 'do_not_reply@sendgrid.com'
+  default :from => 'do_not_reply@example.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_job_done_email(user)
