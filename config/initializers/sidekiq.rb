7cafa39b4b6c55f9cae1afde973b4778db7e5b6f
Eric Dahl <eric@skeyelab.com>
2016-11-09 14:41:53 -0600

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,8 @@ require 'autoscaler/heroku_platform_scaler'
 
 Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
-    chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuPlatformScaler.new
+    heroku = Autoscaler::HerokuScaler.new
+    chain.add Autoscaler::Sidekiq::Client, 'files'=>heroku, 'rows'=>heroku, 'default'=>heroku
   end
 end
 
