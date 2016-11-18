34256e586a35ece90a4d188e741f74868c2bbfec
Eric Dahl <eric@skeyelab.com>
2016-11-18 12:42:30 -0600

    concurrency

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,7 +8,7 @@ if ENV['HEROKU_ACCESS_TOKEN'] and ENV['HEROKU_APP']
     config.client_middleware do |chain|
       heroku   = Autoscaler::HerokuPlatformScaler.new
       strategy = Autoscaler::DelayedShutdown.new(
-        Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY']),
+        Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY'].to_i),
         10
       )
       Autoscaler::Sidekiq::Client
@@ -20,7 +20,7 @@ if ENV['HEROKU_ACCESS_TOKEN'] and ENV['HEROKU_APP']
   Sidekiq.configure_server do |config|
     config.server_middleware do |chain|
       strategy = Autoscaler::DelayedShutdown.new(
-        Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY']),
+        Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY'].to_i),
         10
       )
       chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, strategy)
