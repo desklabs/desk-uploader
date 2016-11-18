db29036dd1e66959ff5fc35a4a2159912a6c9a3a
Eric Dahl <eric@skeyelab.com>
2016-11-18 12:41:30 -0600

    shorter timeout

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,7 +9,7 @@ if ENV['HEROKU_ACCESS_TOKEN'] and ENV['HEROKU_APP']
       heroku   = Autoscaler::HerokuPlatformScaler.new
       strategy = Autoscaler::DelayedShutdown.new(
         Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY']),
-        60
+        10
       )
       Autoscaler::Sidekiq::Client
       .add_to_chain(chain, 'files'=>heroku, 'rows'=>heroku, 'default'=>heroku)
@@ -21,7 +21,7 @@ if ENV['HEROKU_ACCESS_TOKEN'] and ENV['HEROKU_APP']
     config.server_middleware do |chain|
       strategy = Autoscaler::DelayedShutdown.new(
         Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY']),
-        60
+        10
       )
       chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, strategy)
     end
