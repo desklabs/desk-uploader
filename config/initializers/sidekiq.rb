d08c3fc70cf7960388649d2bb4b22903c7811528
Eric Dahl <eric@skeyelab.com>
2016-11-18 12:40:00 -0600

    scaling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,13 +3,13 @@ require 'autoscaler/heroku_platform_scaler'
 require 'autoscaler/linear_scaling_strategy'
 
 
-if ENV['HEROKU_API_KEY'] and ENV['HEROKU_APP']
+if ENV['HEROKU_ACCESS_TOKEN'] and ENV['HEROKU_APP']
   Sidekiq.configure_client do |config|
     config.client_middleware do |chain|
       heroku   = Autoscaler::HerokuPlatformScaler.new
       strategy = Autoscaler::DelayedShutdown.new(
         Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY']),
-        10
+        60
       )
       Autoscaler::Sidekiq::Client
       .add_to_chain(chain, 'files'=>heroku, 'rows'=>heroku, 'default'=>heroku)
@@ -21,7 +21,7 @@ if ENV['HEROKU_API_KEY'] and ENV['HEROKU_APP']
     config.server_middleware do |chain|
       strategy = Autoscaler::DelayedShutdown.new(
         Autoscaler::LinearScalingStrategy.new(5, ENV['SIDEKIQ_CONCURRENCY']),
-        10
+        60
       )
       chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuPlatformScaler.new, strategy)
     end
