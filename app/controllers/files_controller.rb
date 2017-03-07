57d4a2b2654b52daddf1e06d7e9e4ae9344b7869
Eric Dahl <eric@skeyelab.com>
2017-03-07 12:18:23 -0600

    files controller update

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -7,7 +7,20 @@ class FilesController < ApplicationController
 
   def import
     Redis.new.ping
-    num_imported = UploadedCsvFile.import(params[:file], params[:domain], params[:username], params[:password], params[:filetype])
+
+    num_imported = UploadedCsvFile.import(
+      params[:file],
+      params[:domain],
+      params[:username],
+      params[:password],
+      params[:filetype],
+      params[:token],
+      params[:token_secret],
+      params[:consumer_key],
+      params[:consumer_secret],
+      params[:auth_type]
+
+    )
     redirect_to root_url, notice: "CSV queued and is uploading in the background.  You will receive an email when finished."
   end
 end
